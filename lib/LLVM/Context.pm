b8beaef2240cc12f15b3d807147e97f096e7ce83
Alessandro Ghedini <alexbio@cpan.org>
2012-07-22 11:27:21 +0000

    Import of ALEXBIO/LLVM-0.01 from CPAN.
    gitpan-cpan-distribution: LLVM
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ALEXBIO/LLVM-0.01.tar.gz
    gitpan-cpan-author:       ALEXBIO
    gitpan-cpan-maturity:     released

---
+++ lib/LLVM/Context.pm
@@ -0,0 +1,43 @@
+package LLVM::Context;
+{
+  $LLVM::Context::VERSION = '0.01';
+}
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+LLVM::Context - LLVM context class
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+Some description here...
+
+=head1 METHODS
+
+=head2 new( )
+
+Create a new C<LLVM::Context> object.
+
+=head1 AUTHOR
+
+Alessandro Ghedini <alexbio@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Alessandro Ghedini.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1; # End of LLVM::Context
